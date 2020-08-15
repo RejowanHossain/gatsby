e18ae8b8b66ee21303af8419c1fbe25033b253f9
RejowanHossain <rezwan.hossain@outlook.com>
2020-08-15 17:09:55 +0600

    completed hamburger animation

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('.hamburger');
+const overlay = document.querySelector('#list');
+const body = document.querySelector('body');
+
+hamburger.addEventListener('click', function () {
+    if (overlay.classList == 'menu-list') {
+        overlay.classList.add('tog');
+
+        //adding class for animating the hamburger 
+        hamburger.classList.add('click');
+        hamburger.classList.add('click2');
+    } else {
+        overlay.classList.remove('tog');
+
+        //hamburger animation remove
+        hamburger.classList.remove('click');
+        hamburger.classList.remove('click2');
+    }
+})
